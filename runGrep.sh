e7299297128b788c1f9d8bccf845a28ad30981ba
Tarik Rashada <trashada1@gmail.com>
2025-06-28 17:13:47 -0400

    chore: added gitignore to ignore compiled binaries

--- runGrep.sh
+++ runGrep.sh
@@ -1,16 +1,6 @@
 set -e # Exit early if any commands fail
-
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
   go build -o /tmp/goGrep/*.go
 )
-
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
 exec /tmp/goGrep "$@"
